7494a85185ec75ce609683556bdd700219a86ada
Charles J. C. Elling Espejel <charles@denumeris.com>
2016-06-15 13:21:52 -0500

    Add configuration to exclude tables/views from purge.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,38 @@
+<?php
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * 
+ * @author Denumeris, Jun 15, 2016
+ * @author Charles J. C. Elling, Jun 15, 2016
+ *
+ */
+class Configuration implements ConfigurationInterface{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root('data-fixtures');
+		$rootNode->children()
+			->arrayNode('orm')
+				->addDefaultsIfNotSet()
+				->children()
+					->arrayNode('excluded')
+						->treatNullLike(array())
+						->defaultValue(array())
+						->prototype('scalar')->end()
+					->end()
+				->end()
+			->end()
+		->end();
+	
+		// Here you should define the parameters that are allowed to
+		// configure your bundle. See the documentation linked above for
+		// more information on that topic.
+	
+		return $treeBuilder;
+	}
+}
---
+++ DependencyInjection/DoctrineFixturesExtension.php
@@ -0,0 +1,28 @@
+<?php
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+/**
+ * 
+ * @author Denumeris, Jun 15, 2016
+ * @author Charles J. C. Elling, Jun 15, 2016
+ *
+ */
+class DoctrineFixturesExtension extends Extension{
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function load(array $configs, ContainerBuilder $container)
+	{
+		$configuration = new Configuration();
+		$config = $this->processConfiguration($configuration, $configs);
+	
+		$container->setParameter('doctrine.data-fixtures.orm.excluded', $config['data-fixtures']['orm']['excluded']);
+		/*
+		$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yml');
+		*/
+	}
+}
